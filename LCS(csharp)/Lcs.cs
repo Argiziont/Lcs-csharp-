6b596724c115618811d2261f990078863af1d334
Argiziont <tools33333@gmail.com>
2020-04-27 19:50:29 +0300

    Added separate lsc class

---
+++ LCS(csharp)/Lcs.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LCS_csharp_
+{
+    class Lcs
+    {
+        public string FirstString { get; }
+        public string SecondString { get; }
+
+        private int[,] lcsTable;
+
+        private char[] lcsAnswer;
+
+
+        public Lcs(string s1, string s2)
+        {
+            FirstString = s1;
+            SecondString = s2;
+        }
+
+        private void TableSet()
+        {
+            //define lcs table (numbers)
+            lcsTable = new int[FirstString.Length + 1, SecondString.Length + 1]; ;
+
+            for (int itt = 0; itt <= FirstString.Length; itt++)
+            {
+                for (int jtt = 0; jtt <= SecondString.Length; jtt++)
+                {
+                    //first row, fist column- fill with 0
+                    if (itt == 0 || jtt == 0)
+                        lcsTable[itt, jtt] = 0;
+                    //if character of current row and current column  are the same fill by +1 to diagonal
+                    else if (FirstString[itt - 1] == SecondString[jtt - 1])
+                        lcsTable[itt, jtt] = lcsTable[itt - 1, jtt - 1] + 1;
+                    //else take the maximum value from the previous column and previous row
+                    else
+                        lcsTable[itt, jtt] = Math.Max(lcsTable[itt - 1, jtt], lcsTable[itt, jtt - 1]);
+                }
+            }
+        }
+        private void LcsFinder()
+        {
+            TableSet();
+
+            int index = lcsTable[FirstString.Length, SecondString.Length];
+            //define lcs table (characters)
+            lcsAnswer = new char[index + 1];
+            lcsAnswer[index] = '\0';
+            int i = FirstString.Length, j = SecondString.Length;
+            //going backward and finding diagonal elements
+            while (i > 0 && j > 0)
+            {
+                if (FirstString[i - 1] == SecondString[j - 1])
+                {
+                    lcsAnswer[index - 1] = FirstString[i - 1];
+                    i--;
+                    j--;
+                    index--;
+                }
+                else if (lcsTable[i - 1, j] > lcsTable[i, j - 1])
+                    i--;
+                else
+                    j--;
+            }
+
+        }
+
+        public void LcsOut()
+        {
+            LcsFinder();
+            Console.WriteLine($"\nS1 : {FirstString}\nS2 : {SecondString}\nLCS: {new string(lcsAnswer)} ");
+        }
+
+    }
+}
--- LCS(csharp)/Program.cs
+++ LCS(csharp)/Program.cs
@@ -5,63 +5,21 @@ namespace LCS_csharp_
 {
     class Program
     {
-        static void Lcs(string s1, string s2)
-        {
-            var s1len = s1.Length;
-            var s2len = s2.Length;
-            //define lcs table (numbers)
-            int[,] lcsTable = new int[s1len + 1, s2len + 1];
-
-            for (int itt = 0; itt <= s1len; itt++)
-            {
-                for (int jtt = 0; jtt <= s2len; jtt++)
-                {
-                    //first row, fist column- fill with 0
-                    if (itt == 0 || jtt == 0)
-                        lcsTable[itt, jtt] = 0;
-                    //if character of current row and current column  are the same fill by +1 to diagonal
-                    else if (s1[itt - 1] == s2[jtt - 1])
-                        lcsTable[itt, jtt] = lcsTable[itt - 1, jtt - 1] + 1;
-                    //else take the maximum value from the previous column and previous row
-                    else
-                        lcsTable[itt, jtt] = Math.Max(lcsTable[itt - 1, jtt], lcsTable[itt, jtt - 1]);
-                }
-            }
-
-            int index = lcsTable[s1len, s2len];
-            //define lcs table (characters)
-            char[] lcs = new char[index + 1];
-            lcs[index] = '\0';
-            int i = s1len, j = s2len;
-            //going backward and finding diagonal elements
-            while (i > 0 && j > 0)
-            {
-                if (s1[i - 1] == s2[j - 1])
-                {
-                    lcs[index - 1] = s1[i - 1];
-                    i--;
-                    j--;
-                    index--;
-                }
-                else if (lcsTable[i - 1, j] > lcsTable[i, j - 1])
-                    i--;
-                else
-                    j--;
-            }
-            Console.WriteLine($"\nS1 : {s1}\nS2 : {s2}\nLCS: {new string(lcs)} ");
-        }
-
         static void Main()
         {
-
             while (true)
             {
-                Console.WriteLine("Program for solving LCS problem \n Please enter 2 string separated with ',' ");
+                Console.Write("Program for solving LCS problem \n Please enter 2 string separated with ',' : ");
                 var input = Console.ReadLine();
                 string[] words = input.Split(',');
                 if (words.Length == 2 && input.All(c => Char.IsLetterOrDigit(c) || c == ','))
                 {
-                    Lcs(words[0], words[1]);
+                    Lcs lcs = new Lcs(words[0], words[1]);
+
+                    Console.WriteLine("\nYour answer\n" + new string('-' , 25));
+                    lcs.LcsOut();
+                    Console.WriteLine("\n" + new string('-', 25));
+
                     Console.ReadKey();
                     Environment.Exit(0);
                 }
